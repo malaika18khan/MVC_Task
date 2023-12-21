f93aef2cf46b1dbeacb68b631e5dbaf32fea21a8
your_username <your_email>
2023-12-21 10:37:27 +0500

    Added room database

--- app/src/main/java/com/example/mvctask/MainActivity.kt
+++ app/src/main/java/com/example/mvctask/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.mvctask
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn_signin = findViewById<TextView>(R.id.sign_in_btn)
+
+        btn_signin.setOnClickListener {
+            val intent = Intent(this, LoginScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
